a2d885d5ab3453130f8341455764e04ab69ee4ba
League Student <league@iMac-20.attlocal.net>
2020-01-26 12:54:14 -0800

    Robot Race, Hidden Button

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,11 +1,13 @@
 package arrays;
 
+import java.util.Random;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
 	//1. make a main method
 public static void main(String[] args) {
-	
+	boolean top = false;
 
 	//2. create an array of 5 robots.
 	Robot[] robots = new Robot [5];
@@ -16,17 +18,27 @@ for(int i = 0; i<robots.length; i++) {
 		//4. make each robot start at the bottom of the screen, side by side, facing up
 robots[i].setY(490);
 robots[i].setX((i+1)*140);
+robots[i].setSpeed(150);
 }
 	//5. Use another for loop to iterate through the array and make each robot move 
 	//   a random amount less than 50.
+//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
+while(top==false) {
 for(int o = 0; o<robots.length; o++) {
-	
+Random randy = new Random();	
+int x = randy.nextInt(50);
+robots[o].move(x);
+
+if(robots[o].getY()<=0) {
+	 top = true;
+	 System.out.println(o);
 }
-	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
+}
+}	
 
 	//7. declare that robot the winner and throw it a party!
 
-	//8. try different races with different amounts of robots.
+	//8. optional: try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
 
